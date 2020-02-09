4cc7b64cc5a99b938018b6bdf8db7d59a65ffd38
Uffik81 <ufandeev81@gmail.com>
2020-02-09 21:54:04 +0300

    Update

--- casher.py
+++ casher.py
@@ -3,22 +3,10 @@
 import sys
 import udevices.managerdevice as mdevice
 import udevices.shtrihm 
-import uforms.ufrmhelp as helpinfo
-import uforms.ufrmauth as frmauth
-from tkinter import *
+import uforms.ufrmhelp as helpinfo 
 
-def startForm():
-    root = Tk()
-    if sys.platform != 'linux2':
-        root.wm_state('zoomed')
-    else:
-        root.wm_attributes('-zoomed', True)
-    
-    authFrm = frmauth.FrmAuth(root)
-    root.mainloop()
 
 if __name__ == "__main__":
-    #listdev = mdevice.UmanagerDevices()
-    #helpinfo.showinfo()
-    startForm()
+    listdev = mdevice.UmanagerDevices()
+    helpinfo.showinfo()
     pass
